560b2992d821a5758e68d51bf51f20c9ed4a0c73
Kevin Ortiz <kevinortiz@Kevins-MacBook-Pro.local>
2025-02-07 16:12:29 -0500

    Latest

--- django/pokemon_tracker_project/pokemon_ebay_tracker/views.py
+++ django/pokemon_tracker_project/pokemon_ebay_tracker/views.py
@@ -5,8 +5,8 @@ from .scripts import pokemon_api_helper
 
 @require_GET
 def load_data(request):
-    query_param = request.GET.get('ebay_api_key')
-    cards_info = pokemon_api_helper.fetch_pokemon_cards()  # Run the Python script to generate data
+    ebay_api_key = request.GET.get('ebay_api_key')
+    cards_info = pokemon_api_helper.fetch_pokemon_cards(ebay_api_key)  # Run the Python script to generate data
     return JsonResponse({'cards_info': cards_info})
 
 def home(request):
