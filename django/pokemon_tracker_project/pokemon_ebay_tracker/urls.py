8ea285a000778b05f0bc8cebee4b5ed9e71ba8fa
Kevin Ortiz <kevinortiz@Kevins-MacBook-Pro.local>
2025-02-07 14:34:27 -0500

    Adding latest code

--- django/pokemon_tracker_project/pokemon_ebay_tracker/urls.py
+++ django/pokemon_tracker_project/pokemon_ebay_tracker/urls.py
@@ -4,5 +4,6 @@ from . import views
 app_name = 'pokemon_ebay_tracker'
 
 urlpatterns = [
-    path('', views.home, name='home'),
+    path('', views.home, name='home'),  # Homepage view
+    path('load_data/', views.load_data, name='load_data'),  # AJAX data request
 ]
--- django/pokemon_tracker_project/pokemon_ebay_tracker/views.py
+++ django/pokemon_tracker_project/pokemon_ebay_tracker/views.py
@@ -1,6 +1,13 @@
 from django.shortcuts import render
+from django.http import JsonResponse
+from django.views.decorators.http import require_GET
 from .scripts import pokemon_api_helper
 
+@require_GET
+def load_data(request):
+    query_param = request.GET.get('ebay_api_key')
+    cards_info = pokemon_api_helper.fetch_pokemon_cards()  # Run the Python script to generate data
+    return JsonResponse({'cards_info': cards_info})
+
 def home(request):
-    cards_info = pokemon_api_helper.fetch_pokemon_cards()  # Call the function to get data
-    return render(request, 'home.html', {'cards_info': cards_info})
+    return render(request, 'home.html')
