32b382b9ffd6c0c99078dc00ce6def0fd974d118
Kevin Ortiz <kevinortiz@Kevins-MacBook-Pro.local>
2025-02-08 16:15:52 -0500

    Working

--- django/pokemon_tracker_project/pokemon_ebay_tracker/scripts/pokemon_api_helper.py
+++ django/pokemon_tracker_project/pokemon_ebay_tracker/scripts/pokemon_api_helper.py
@@ -1,27 +1,44 @@
 import datetime
 import requests
-import json
 
-def fetch_pokemon_cards(ebay_api_key, tcg_player_cards):
-    query_end_time = (datetime.datetime.now(datetime.timezone.utc) + datetime.timedelta(hours=1)).strftime('%Y-%m-%dT%H:%M:%S') + 'Z'
+def fetch_pokemon_cards(
+        ebay_api_key: str,
+        tcg_player_cards: dict,
+        graded_check: str,
+        set_to_check: str,
+        minimum_bid_price: int,
+        maximum_bid_percentage: float,
+        time_left_hours: int
+    ):
+    query_end_time = (datetime.datetime.now(datetime.timezone.utc) + datetime.timedelta(hours=int(time_left_hours))).strftime('%Y-%m-%dT%H:%M:%S') + 'Z'
 
-    sets_to_check = [
-        "Prismatic Evolutions",
-        "Surging Sparks",
-        "Crown Zenith",
-        "Crown Zenith Galarian Gallery",
-        "Evolving Skies",
-        "Paldea Evolved",
-        "151",
-        "Obsidian Flames",
-        "Vivid Voltage",
-        "Silver Tempest",
-        "Astral Radiance",
-        "Brilliant Stars",
-        "Twilight Masquerade",
-        "Paldean Fates",
-        "Paradox Rift",
-    ]
+    if set_to_check != "All":
+        sets_to_check = [set_to_check]
+    else:
+        sets_to_check = [
+            "Prismatic Evolutions",
+            "Surging Sparks",
+            "Crown Zenith",
+            "Crown Zenith Galarian Gallery",
+            "Evolving Skies",
+            "Paldea Evolved",
+            "151",
+            "Obsidian Flames",
+            "Vivid Voltage",
+            "Silver Tempest",
+            "Astral Radiance",
+            "Brilliant Stars",
+            "Twilight Masquerade",
+            "Paldean Fates",
+            "Paradox Rift",
+        ]
+
+    if graded_check == "Graded":
+        condition_ids = "{2750}"
+    elif graded_check == "Ungraded":
+        condition_ids = "{4000}"
+    elif graded_check == "All":
+        condition_ids = "{2750|4000}"
 
     cards_info = []
 
@@ -36,19 +53,20 @@ def fetch_pokemon_cards(ebay_api_key, tcg_player_cards):
                     "Content-Type": "application/json"
                 }
 
+                maximum_bid_price = int(float(card.get('market')) * float(maximum_bid_percentage))
                 params = {
                     "q": f"{card.get('name')} {card.get('number')}/{card.get('printed_total')}",
                     "filter": [
                         "buyingOptions:{AUCTION}",
-                        f"price:[5..{card.get('market') * 0.8:.2f}]",
                         "priceCurrency:USD",
-                        f"itemEndDate:[..{query_end_time}]",
-                        "conditionIds:{2750}",
                         "deliveryCountry:US",
+                        f"price:[{minimum_bid_price}..{maximum_bid_price}]", 
+                        f"itemEndDate:[..{query_end_time}]",
+                        f"conditionIds:{condition_ids}",
                     ]
                 }
-
                 response = requests.get(url, headers=headers, params=params)
+                print(response.json())
 
                 if response.status_code == 200:
                     response_dict = response.json()
@@ -62,7 +80,7 @@ def fetch_pokemon_cards(ebay_api_key, tcg_player_cards):
                             if response.status_code == 200:
                                 response = response.json()
 
-                                if response.get("seller").get("feedbackPercentage") == "0.0":
+                                if response.get("seller").get("feedbackPercentage") < "95.0":
                                     continue
 
                                 end_time = datetime.datetime.strptime(response.get("itemEndDate"), '%Y-%m-%dT%H:%M:%S.%fZ')
--- django/pokemon_tracker_project/pokemon_ebay_tracker/views.py
+++ django/pokemon_tracker_project/pokemon_ebay_tracker/views.py
@@ -6,8 +6,21 @@ from .scripts import pokemon_api_helper, tcgplayer_cards_info
 @require_GET
 def load_data(request):
     ebay_api_key = request.GET.get('ebay_api_key')
+    graded_check = request.GET.get('graded_check', "All")
+    set_to_check = request.GET.get('set_to_check', "All")
+    minimum_bid_price = request.GET.get('min_bid_price', 10)
+    maximum_bid_percentage = request.GET.get('max_bid_percentage', 0.8)
+    time_left_hours = request.GET.get('time_left_hours', 1)
     tcg_player_cards = tcgplayer_cards_info.cards_info
-    cards_info = pokemon_api_helper.fetch_pokemon_cards(ebay_api_key, tcg_player_cards)  # Run the Python script to generate data
+    cards_info = pokemon_api_helper.fetch_pokemon_cards(
+        ebay_api_key=ebay_api_key,
+        tcg_player_cards=tcg_player_cards,
+        graded_check=graded_check,
+        set_to_check=set_to_check,
+        minimum_bid_price=minimum_bid_price,
+        maximum_bid_percentage=maximum_bid_percentage,
+        time_left_hours=time_left_hours
+    ) 
     return JsonResponse({'cards_info': cards_info})
 
 def home(request):
